4066b56483733733ee45312bfe92bd65a4f9fc47
lengocthien <sangbomvip@gmail.com>
2023-10-05 17:13:05 +0700

    nop bai

--- project/src/components/discount/Ddata.js
+++ project/src/components/discount/Ddata.js
@@ -1,47 +1,47 @@
 const Ddata = [
     {
       cover: "./images/discount/bt3.png",
-      name: "BenuX 2022",
+      name: "Bông tai hottrend",
       price: "$250",
     },
     {
       cover: "./images/discount/dc-1.png",
-      name: "Sony TV 1080p",
+      name: "Dây chuyền",
       price: "$450",
     },
     {
       cover: "./images/discount/bt1.png",
-      name: "Sony PS4",
+      name: "Bông tai nữ tính",
       price: "$50",
     },
     {
       cover: "./images/discount/lt5.png",
-      name: "Setgearr 2022",
+      name: "Lắc tay 2022",
       price: "$100",
     },
     {
       cover: "./images/discount/n-3.png",
-      name: "Tony BGB",
+      name: "Nhẫn cầu hôn",
       price: "$20",
     },
     {
       cover: "./images/discount/vt1.png",
-      name: "RG products",
+      name: "Vòng tay",
       price: "$200",
     },
     {
       cover: "./images/discount/vt2.png",
-      name: "Ranasonic 2022",
+      name: "Vòng tay 2022",
       price: "$300",
     },
     {
       cover: "./images/discount/vt4.png",
-      name: "Pune HD",
+      name: "vòng tay Pune",
       price: "$30",
     },
     {
       cover: "./images/discount/vt6.png",
-      name: "Sony CCTV",
+      name: "Vòng tay CCTV",
       price: "$80",
     },
   ]
--- project/src/components/top/TopCart.js
+++ project/src/components/top/TopCart.js
@@ -1,38 +1,120 @@
-import React from "react"
+// import React from "react"
+// import Slider from "react-slick"
+// import "slick-carousel/slick/slick.css"
+// import "slick-carousel/slick/slick-theme.css"
+// import Tdata from "./Tdata"
+
+// const TopCart = () => {
+//   const settings = {
+//     dots: false,
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//   }
+//   return (
+//     <>
+//       <Slider {...settings}>
+//         {Tdata.map((value, index) => {
+//           return (
+//             <>
+//               <div className='box product' key={index}>
+//                 <div className='nametop d_flex'>
+//                   <span className='tleft'>{value.para}</span>
+//                   <span className='tright'>{value.desc}</span>
+//                 </div>
+//                 <div className='img'>
+//                   <img src={value.cover} alt='' />
+//                 </div>
+//               </div>
+//             </>
+//           )
+//         })}
+//       </Slider>
+//     </>
+//   )
+// }
+
+// export default TopCart
+
 import Slider from "react-slick"
+
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import Tdata from "./Tdata"
+import categoryservice from "../../services/CategoryServies"
+import { useEffect, useState } from "react";
+import { urlImage } from "../../config";
 
-const TopCart = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-  }
+function Categories() {
+  // const Categories = () => {
+    const settings = {
+     dots: false,
+       infinite: true,
+       slidesToShow: 3,
+       slidesToScroll: 1,
+       autoplay: true,
+     }
+  const [categorys, setCategorys] = useState([]);
+    useEffect(function () {
+        (async function () {
+            try {
+                const result = await categoryservice.getAll();
+                setCategorys(result.data.categorys)
+            }
+            catch (error) {
+                console.error(error);
+            }
+
+        })();
+    }, []);
+    
+
+  // categoryservice.getById(id);
   return (
-    <>
-      <Slider {...settings}>
-        {Tdata.map((value, index) => {
+  
+          // <>
+          //    <div className='category'>
+          //      {categorys.map((value, index) => {
+          //        return (
+          //          <div className='box f_flex' key={index}>
+          //            {/* <img src={value.image} alt='' /> */}
+
+          //            <img
+          //         className="img-fluid"
+          //         src={urlImage + "category/" + value.image}
+          //         />
+
+
+          //            <span>{value.name}</span>
+          //          </div>
+          //        )
+          //      })}
+          //    </div>
+          //  </>
+          <Slider {...settings}>
+        {categorys.map((value, index) => {
           return (
-            <>
-              <div className='box product' key={index}>
-                <div className='nametop d_flex'>
-                  <span className='tleft'>{value.para}</span>
-                  <span className='tright'>{value.desc}</span>
-                </div>
-                <div className='img'>
-                  <img src={value.cover} alt='' />
-                </div>
-              </div>
-            </>
-          )
-        })}
-      </Slider>
-    </>
-  )
+             <>
+               <div className='box product' key={index}>
+                 <div className='nametop d_flex'>
+                   <span className='tleft'>{value.name}</span>
+                   <span className='tright'>{value.desc}</span>
+                 </div>
+                 <img
+                   className="img-fluid"
+                   src={urlImage + "category/" + value.image}
+                   />
+                 {/* <div className='img'>
+                   <img src={value.cover} alt='' />
+                 </div> */}
+               </div>
+             </>
+           )
+         })}
+       </Slider>
+         
+  );
 }
 
-export default TopCart
+
+export default Categories;
